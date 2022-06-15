237b5958cac53722509d30adbf4ef289b388a2f1
omar01550 <arom57818@gmail.com>
2022-06-15 21:56:35 +0200

    add project

---
+++ faq-accordion-card-main/main.js
@@ -0,0 +1,68 @@
+//setting bars
+let setting=document.querySelector('.setting');
+let settingIcon=document.querySelector('.fa-bars');
+let closeIcon=document.querySelector('.close-icon');
+
+handelSetting();
+
+
+
+let btn=document.querySelector(".q button");
+let p=document.querySelector(".answer");
+
+
+
+
+
+document.documentElement.addEventListener('click',function(e){
+  if(e.target.classList.contains("show")){
+     let parentId=e.target.parentNode.parentNode.id;
+     let text=document.querySelector(`#${parentId} p`)
+
+      text.classList.toggle('hidden');
+
+
+
+
+
+
+  }
+});
+
+
+
+//add new qs
+let addNew=document.querySelector('.add-new');
+addNew.oclick=function() {
+   console.log(true);
+
+}
+
+setting.style.right="-100%";
+//functions
+function handelSetting() {
+     settingIcon.onclick=function(){
+        setting.style.right="0";
+     }
+
+      closeIcon.onclick=function(){
+           setting.style.right="-100%";
+      }
+
+
+}
+
+
+let thems=document.querySelector('.thems');
+let colorBolet=document.querySelectorAll('.thems li');
+themsPage();
+function themsPage(){
+    colorBolet.forEach((ele, i) => {
+         ele.onclick=function(){
+            console.log(documentElement.style);
+         }
+
+         
+    });
+
+}
